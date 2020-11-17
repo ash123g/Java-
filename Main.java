76d4e3ca01be97c4a0af9ed6c3566703e4d9a69e
ash123g <ashishgarg5680@gmail.com>
2020-11-17 12:06:42 +0530

    Add the new code

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		
+	int n= 5,ans=1;
+	
+	for (int i=5; i>=1; i--){
+	    ans*=i;
+	    
+	}
+		System.out.println(ans);
+		
+	}
+}
